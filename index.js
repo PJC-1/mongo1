2aabdd1fd530f213a186cd8833e41e756cfb4254
PJC-1 <jumpzine@gmail.com>
2017-08-13 13:43:01 -0700

    installed pug templating engine LOCAL SUCCESS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 var url = 'mongodb://localhost:27017/jade';
 
@@ -14,12 +15,19 @@ db.on('error', function(err){
     console.log(err);
 });
 
+// Init App
 const app = express();
 
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Home Route
 app.get('/', function(req, res){
-    res.send('Hello World');
+    res.render('index');
 });
 
+// Start Server
 app.listen(process.env.PORT || 3000, function(){
     console.log('You are listening to the smooth sounds of port 3000...');
 });
