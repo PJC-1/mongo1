348e3b3693db3df5e132a0188fe52ad1d80d7232
PJC-1 <jumpzine@gmail.com>
2017-08-13 12:56:48 -0700

    replaced MongoClient with mongoose to make connection to the db. SUCCESS

--- README.md
+++ README.md
@@ -2,6 +2,27 @@ test deploy heroku with mlab
 
 notes:
 
+MONGODB:
+
+MONGOOSE:
+
+DeprecationWarning: `open()` is deprecated in mongoose >= 4.11.0, use `openUri()` instead:
+Workaround is to "npm remove mongoose" then "npm install mongoose@4.10.8 --save", until they fix 4.11.x.
+
+
+NODE:
+
+EXPRESS:
+
+HEROKU:
+
+to check logs in heroku:
+
+heroku logs
+heroku logs --trail
+
+OTHER:
+
 environment variable setting syntax:
 export varName = varValue
 
--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
+const mongoose = require('mongoose');
 
 const app = express();
 
 var url = 'mongodb://localhost:27017/jade';
-process.env.MONGODB_URI
 
-MongoClient.connect(process.env.MONGODB_URI || url, function(err, db){
-    if(err){
-        console.log('Unable to connect to the mongoDB server. Error:', err);
-    } else {
-        console.log('Connection established to the DB.');
-        db.close();
-    }
+mongoose.connect(process.env.MONGODB_URI || url);
+let db = mongoose.connection;
+
+db.once('open', function(){
+    console.log('Connected');
 });
+
+db.on('error', function(err){
+    console.log(err);
+});
+
+// MongoClient.connect(process.env.MONGODB_URI || url, function(err, db){
+//     if(err){
+//         console.log('Unable to connect to the mongoDB server. Error:', err);
+//     } else {
+//         console.log('Connection established to the DB.');
+//         db.close();
+//     }
+// });
 
 app.get('/', function(req, res){
     res.send('Hello World');
