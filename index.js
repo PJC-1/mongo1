4a453696557ab72892d11046f0c4a2211a17ec0b
PJC-1 <jumpzine@gmail.com>
2017-08-14 21:13:54 -0700

    created a local database and am now successfully pulling data from it

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const path = require('path');
 var url = 'mongodb://localhost:27017/jade';
 
 mongoose.connect(process.env.MONGODB_URI || url);
+// mongoose.connect(url);
+
 let db = mongoose.connection;
 
+// Check connection
 db.once('open', function(){
     console.log('Connected');
 });
 
+// Check for DB errors
 db.on('error', function(err){
     console.log(err);
 });
@@ -18,35 +22,24 @@ db.on('error', function(err){
 // Init App
 const app = express();
 
+// Bring in Models
+let Article = require('./models/article');
+
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // Home Route
 app.get('/', function(req, res){
-    let articles = [
-        {
-            id:1,
-            title:'Article One',
-            author:'PJC1',
-            body:'This is article one'
-        },
-        {
-            id:2,
-            title:'Article Two',
-            author:'McDowell',
-            body:'This is article two'
-        },
-        {
-            id:3,
-            title:'Article Three',
-            author:'Duckett',
-            body:'This is article three'
+    Article.find({}, function(err, articles){
+        if(err){
+            console.log(err);
+        } else {
+            res.render('index', {
+              title:'Articles',
+              articles: articles
+            });
         }
-    ];
-    res.render('index', {
-        title:'Articles',
-        articles: articles
     });
 });
 
