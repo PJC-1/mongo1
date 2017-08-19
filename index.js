a9205caa3156639867e6a1072f9adaef6527169a
PJC-1 <jumpzine@gmail.com>
2017-08-18 22:51:58 -0700

    log out functionality SUCCESS

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const flash = require('connect-flash');
 const session = require('express-session');
+const passport = require('passport');
 const config = require('./config/database');
 
 var url = config.database;
@@ -74,6 +75,17 @@ app.use(expressValidator({
     };
   }
 }));
+
+// Passport Config
+require('./config/passport')(passport);
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('*', function(req, res, next){
+    res.locals.user = req.user || null;
+    next();
+});
 
 // Home Route
 app.get('/', function(req, res){
